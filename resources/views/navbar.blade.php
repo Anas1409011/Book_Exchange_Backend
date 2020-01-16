164e79959cf82c7ad32657bfc2a69655cd9d45de
Anas1409011 <58006505+Anas1409011@users.noreply.github.com>
2020-01-16 05:42:30 -0600

    Add files via upload

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,12 @@
+
+
+    <div class="container">
+      <div class="row">
+         <ul class="nav nav-tabs" style="padding-left: 10px; font-size:20px;">
+    		<li style="padding-right: 20px;"><a href="{{url('/dashboard')}}"> Dashboard </a></li>
+    		<li style="padding-right: 20px;"><a href="{{URL::to('/checkout')}}"> All Users</a></li>
+    		<li style="padding-right: 20px;"><a href="{{URL::to('/add_to_cart')}}">All Products</a></li>
+    		<li style="padding-right: 20px;"><a href="{{URL::to('/contact_us')}}">Others</a></li>
+         </ul>
+       </div>
+     </div>
